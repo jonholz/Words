377ec7994e4f91400a56b4de8a367c2d67d90eb2
Jonathan Holzman <holz@mail.com>
2018-09-01 13:59:08 -0500

    cleanup code, add debug print statements with compiler directive

--- score.cpp
+++ score.cpp
@@ -5,37 +5,49 @@
 int main(void)
 {
    std::ifstream in("/usr/share/dict/words");
-   if (in.is_open())
+   if (!in.is_open())
    {
-      std::ofstream out("cpp100");
-      if (out.is_open())
-      {
-         int t = 0;
-         std::string l;
-         while(getline(in,l))
-         {
-            int sum = 0;
-            //std::cout << l << std::endl;
-            for (int i = 0; i < l.length(); i++)
-            {
-               if (isupper(l[i]) || (l[i] == 39))
-               {
-                  sum = 0;
-                  break;
-               }
-               sum += tolower(l[i]) - 96;
-               //std::cout << l[i] << " : " << sum << std::endl;
-            }
-            if (sum == 100)
-            {
-               out << l << std::endl;
-               t++;
-            }
-         }
-         std::cout << t << std::endl;
-         out.close();
-      }
-      in.close();
+      std::cerr << "Could not open input file" << std::endl;
+      return -1;
    }
+   std::ofstream out("cpp100");
+   if (!out.is_open())
+   {
+      std::cerr << "Could not open output file" << std::endl;
+      in.close();
+      return -2;
+   }
+   int t = 0;
+   std::string l;
+   while(getline(in,l))
+   {
+      int sum = 0;
+      #ifdef DEBUG
+      std::cout << l << std::endl;
+      #endif
+      for (int i = 0; i < l.length(); i++)
+      {
+         if (isupper(l[i]) || (l[i] == 39))
+         {
+            sum = 0;
+            #ifdef DEBUG
+            std::cout << "Found uppercase or \"'\"" << std::endl;
+            #endif
+            break;
+         }
+         sum += tolower(l[i]) - 96;
+         #ifdef DEBUG
+         std::cout << l[i] << " : " << sum << std::endl;
+         #endif
+      }
+      if (sum == 100)
+      {
+         out << l << std::endl;
+         t++;
+      }
+   }
+   std::cout << t << std::endl;
+   out.close();
+   in.close();
    return 0;
 }
